2d1ee1bfd1e5b3250732c6569f27579b69b89369
Pete Corey <petecorey@gmail.com>
2020-03-28 15:06:23 -0600

    First

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "lodash.product";
+import React from "react";
+import _ from "lodash";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let possibilities = _.chain([
+  ["no root", "root"],
+  ["no 3rd", "b3", "n3"],
+  ["no 5th", "b5", "#5"],
+  ["no 7th", "b7", "n7"],
+  ["no 9th", "b9", "n9", "#9"],
+  ["no 11th", "n11", "#11"],
+  ["no 13th", "b13", "n13"]
+])
+  .thru(a => _.product(...a))
+  .size()
+  .tap(console.log)
+  .value();
+
+const m = 3;
+const M = 4;
+
+const noteNames = [
+  "C",
+  "C#/Db",
+  "D",
+  "D#/Eb",
+  "E",
+  "F",
+  "F#/Gb",
+  "G",
+  "G#/Ab",
+  "A",
+  "A#/Bb",
+  "C"
+];
+
+const templates = {
+  [[m, m]]: "diminished",
+  [[m, M]]: "minor",
+  [[M, m]]: "major",
+  [[M, M]]: "augmented"
+};
+
+const getName = notes => {
+  return _.chain(notes)
+    .map(root =>
+      _.chain(notes)
+        .map(note => (note - root < 0 ? 12 + note - root : note - root))
+        .sort()
+        .value()
+    )
+    .tap(console.log)
+    .map(inversion => [
+      inversion[1] - inversion[0],
+      inversion[2] - inversion[1]
+    ])
+    .tap(console.log)
+    .map((thirds, i) => ({
+      note: noteNames[notes[i]],
+      quality: templates[thirds]
+    }))
+    .reject(({ quality }) => _.isUndefined(quality))
+    .tap(console.log)
+    .value();
+};
+
+const App = () => {
+  return <pre>{JSON.stringify(getName([4, 8, 1, 9]), null, 2)}</pre>;
+};
 
 export default App;
